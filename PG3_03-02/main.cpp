549b24e5bc3398b7f1a61080712980b1ca5d7ab1
iwatakazuya <k021g1122@g.neec.ac.jp>
2023-02-09 13:19:49 +0900

    PG3_3-2再提出

--- PG3_03-02/main.cpp
+++ PG3_03-02/main.cpp
@@ -12,17 +12,17 @@ int main() {
 	Enemy* enemy2 = new Enemy;
 	Enemy* enemy3 = new Enemy;
 
-	cout << Enemy::isAllDead << endl;
-	cout << Enemy::isAllDead << endl;
-	cout << Enemy::isAllDead << endl;
+	cout << enemy1->isAllDead << endl;
+	cout << enemy2->isAllDead << endl;
+	cout << enemy3->isAllDead << endl;
 
 	printf("0:“G‚ªÁ‚¦‚é 1:‚»‚Ì‚Ü‚Ü ==>");
 	scanf_s("%d", &input);
 	if (input == 0)enemy1->isAllDead = false;
 
-	cout << Enemy::isAllDead << endl;
-	cout << Enemy::isAllDead << endl;
-	cout << Enemy::isAllDead << endl;
+	cout << enemy1->isAllDead << endl;
+	cout << enemy2->isAllDead << endl;
+	cout << enemy3->isAllDead << endl;
 
 	delete enemy1;
 	delete enemy2;
